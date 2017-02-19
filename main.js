a7e29e01b1893c7cc71bbf80b0451e9304b13b32
LeonardA-L <leonard@allain-launay.fr>
2017-02-19 14:04:58 -0800

    Use Repeat to keep running tests

--- main.js
+++ main.js
@@ -1,53 +1,8 @@
 var request = require('request');
 var Spooky = require('spooky');
+var Repeat = require('repeat');
 
 var config = require('./config');
-
-console.log('ok')
-
-var spooky = new Spooky({
-    child: {
-    	command: "./node_modules/.bin/casperjs",
-        transport: 'http'
-    },
-    casper: {
-        logLevel: 'debug',
-        verbose: true
-    }
-}, function (err) {
-    if (err) {
-        e = new Error('Failed to initialize SpookyJS');
-        e.details = err;
-        throw e;
-    }
-    console.log('ok2')
-
-    spooky.start(config.productPage);
-
-	spooky.then(function() {
-	    this.echo('First Page: ' + this.getTitle());
-	});
-
-	if (config.size) {
-	    spooky.thenEvaluate(function(config) {
-	        $('#product-select-option-1').val(config.size).change();
-	    }, config);
-	}
-
-	spooky.then(function(){
-	    var isAvailable = this.getHTML('.price') !== 'Sold Out';
-	    this.echo(isAvailable);
-	    this.emit('result', isAvailable);
-	});
-
-	spooky.run();
-});
-
-spooky.on('result', function(result) {
-	if(result) {
-		warnAvailability(config);
-	}
-})
 
 
 function warnAvailability (config) {
@@ -56,8 +11,62 @@ function warnAvailability (config) {
         { json: config.payload },
         function (error, response, body) {
             if(error) {
-            	console.error(error);
+                console.error(error);
             }
         }
     );
 }
+
+function runScript() {
+    var spooky = new Spooky({
+        child: {
+            command: "./node_modules/.bin/casperjs",
+            transport: 'http'
+        },
+        casper: {
+            logLevel: 'debug',
+            verbose: true
+        }
+    },
+    function (err) {
+        if (err) {
+            e = new Error('Failed to initialize SpookyJS');
+            e.details = err;
+            throw e;
+        }
+
+        console.log('Running test')
+        spooky.start(config.productPage);
+
+        spooky.then(function() {
+            this.echo('Reached Page: ' + this.getTitle());
+        });
+
+        if (config.size) {
+            spooky.thenEvaluate(function(config) {
+                $('#product-select-option-1').val(config.size).change();
+            }, config);
+        }
+
+        spooky.then(function(){
+            var isAvailable = this.getHTML('.price') !== 'Sold Out';
+            this.echo(isAvailable);
+            this.emit('result', isAvailable);
+        });
+
+        spooky.run();
+    });
+
+    spooky.on('result', function(result) {
+        console.log('Product available:', result);
+        if(result) {
+            warnAvailability(config);
+        }
+    });
+}
+
+function schedule(){
+    Repeat(runScript).every(5000, 'ms').start.in(1, 'sec');
+}
+
+schedule();
