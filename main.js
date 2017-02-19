d309f91940cf399e2d6fb8922580e8a4833a178e
LeonardA-L <leonard@allain-launay.fr>
2017-02-19 13:47:00 -0800

    Use SpookyJS instead of Casper. Add webhook call

--- main.js
+++ main.js
@@ -1,19 +1,63 @@
-var casper = require('casper').create();
+var request = require('request');
+var Spooky = require('spooky');
 
 var config = require('./config');
 
-casper.start(config.productPage);
+console.log('ok')
 
-if(config.size) {
-	casper.thenEvaluate(function(config) {
-		$('#product-select-option-1').val(config.size).change();
-	}, config);
-}
+var spooky = new Spooky({
+    child: {
+    	command: "./node_modules/.bin/casperjs",
+        transport: 'http'
+    },
+    casper: {
+        logLevel: 'debug',
+        verbose: true
+    }
+}, function (err) {
+    if (err) {
+        e = new Error('Failed to initialize SpookyJS');
+        e.details = err;
+        throw e;
+    }
+    console.log('ok2')
 
-casper.then(function(){
-	// scrape something else
-	var isAvailable = this.getHTML('.price') !== 'Sold Out';
-	this.echo(isAvailable);
+    spooky.start(config.productPage);
+
+	spooky.then(function() {
+	    this.echo('First Page: ' + this.getTitle());
+	});
+
+	if (config.size) {
+	    spooky.thenEvaluate(function(config) {
+	        $('#product-select-option-1').val(config.size).change();
+	    }, config);
+	}
+
+	spooky.then(function(){
+	    var isAvailable = this.getHTML('.price') !== 'Sold Out';
+	    this.echo(isAvailable);
+	    this.emit('result', isAvailable);
+	});
+
+	spooky.run();
 });
 
-casper.run();
+spooky.on('result', function(result) {
+	if(result) {
+		warnAvailability(config);
+	}
+})
+
+
+function warnAvailability (config) {
+    request.post(
+        config.webhook,
+        { json: config.payload },
+        function (error, response, body) {
+            if(error) {
+            	console.error(error);
+            }
+        }
+    );
+}
